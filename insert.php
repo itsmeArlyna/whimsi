5002cdf10e87c2c83b78c2431621e2c4f891378a
arlyn <141389978+itsmeArlynaa@users.noreply.github.com>
2024-04-22 17:27:00 +0800

    update

--- insert.php
+++ insert.php
@@ -14,23 +14,42 @@ if(isset($_POST['buy_now'])){
     $itemName = $_POST["item_name"]; 
     $itemPrice = $_POST["item_price"];
 
+    echo "<script>
+            function showSuccessAlert() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Order Placed Successfully!',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                setTimeout(function(){
+                    window.location.href = 'index.php'; 
+                }, 2000);
+            }
+            
+            function showConfirmation() {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You are about to place an order.',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, place order!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        showSuccessAlert();
+                    } else {
+                        window.location.href = 'index.php';
+                    }
+                });
+            }
+
+            window.onload = showConfirmation;
+          </script>";
+
     $sql = "INSERT INTO orders (name, price) VALUES ('$itemName', '$itemPrice')";
-    if ($db->query($sql) === TRUE) {
-        echo "<script>
-                function showSuccessAlert() {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Order Placed Successfully!',
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                    setTimeout(function(){
-                        window.location.href = 'index.php'; 
-                    }, 1000);
-                }
-                window.onload = showSuccessAlert;
-              </script>";
-    } else {
+    if ($db->query($sql) !== TRUE) {
         echo "Error: " . $sql . "<br>" . $db->error;
     }
 
